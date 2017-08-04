a8ee39a851b2f6fccfa3488fdef464fe89d28cbd
Ryan Goldstein <ryanisaacg@gmail.com>
2017-08-03 22:24:02 -0400

    Added a basic texture drawing test

--- source/app.d
+++ source/app.d
@@ -1,4 +1,4 @@
-import std.stdio;
+import std.stdio, std.file;
 import au;
 
 void main()
@@ -6,9 +6,11 @@ void main()
 	Window engine;
 	WindowConfig config;
 	engine.init("Test title", 640, 480, config);
+	auto tex = engine.loadTexture("test.png");
 	while(engine.should_continue)
 	{
 		engine.begin(black);
+		engine.drawTexture(tex, 0, 0, 32, 32);
 		engine.end();
 	}
 	engine.destroy();
