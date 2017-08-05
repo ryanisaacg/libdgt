b1a7a9bc3165ade09580197506e22d017eea47e6
Ryan Goldstein <ryanisaacg@gmail.com>
2017-08-05 12:33:29 -0400

    Used the OpenGL backend directly to isolate the bug

--- source/app.d
+++ source/app.d
@@ -1,5 +1,6 @@
 import std.stdio, std.file;
 import au;
+import gl_backend;
 
 void main()
 {
@@ -10,11 +11,28 @@ void main()
 	while(engine.should_continue)
 	{
 		engine.begin(black);
+		engine.ctx.add(tex.id,
+			[Vertex(Vectorf(0, 0), Vectorf(0, 0), white),
+			Vertex(Vectorf(32, 0), Vectorf(0, 0), white),
+			Vertex(Vectorf(32, 32), Vectorf(0, 0), white),
+			Vertex(Vectorf(0, 32), Vectorf(0, 0), white)
+
+			],
+			[0, 1, 2, 2, 3, 0]);
+		engine.ctx.add(tex.id, [
+			Vertex(Vectorf(100, 400), Vectorf(0, 0), white),
+			Vertex(Vectorf(132, 400), Vectorf(0, 0), white),
+			Vertex(Vectorf(132, 432), Vectorf(0, 0), white),
+			Vertex(Vectorf(100, 432), Vectorf(0, 0), white)
+			],
+			[0, 1, 2, 2, 3, 0]
+			);
+			/*
 		engine.draw(tex, 100, 0, 32, 32);
 		engine.draw(tex, 100, 400, 32, 32);
 		engine.draw(red, Rectanglef(30, 30, 40, 40));
 		engine.draw(blue, Rectanglef(100, 100, 40, 40));
-		engine.draw(blue, Circlef(100, 100, 32));
+		engine.draw(blue, Circlef(100, 100, 32));*/
 		engine.end();
 	}
 	engine.destroy();
