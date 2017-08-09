cae0ee98967c9bba2ab9e33167024f119f9aa1e1
Ryan Goldstein <ryanisaacg@gmail.com>
2017-08-09 18:26:25 -0400

    Added a function to take a slice of a Texture

--- source/texture.d
+++ source/texture.d
@@ -5,4 +5,12 @@ struct Texture
     uint id;
     int width, height;
     Rectangle!int region;
+
+    @nogc nothrow pure:
+    Texture getSlice(Rectangle!int region)
+    {
+        Texture tex = this;
+        tex.region = region;
+        return tex;
+    }
 }
