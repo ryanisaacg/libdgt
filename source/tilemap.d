4ea862a2ccdecabb3f7818663e2a056d549a67b6
Ryan Goldstein <ryanisaacg@gmail.com>
2017-08-10 00:01:21 -0400

    Updated the tilemap

--- source/tilemap.d
+++ source/tilemap.d
@@ -1,4 +1,5 @@
 import array, geom;
+import std.math;
 
 struct Tile(T)
 {
@@ -8,26 +9,37 @@ struct Tile(T)
 
 struct Tilemap(T)
 {
+	static immutable INVALID_TILE = Tile!T(T(), true);
+
 	private Array!(Tile!T) buffer;
 	private int size = 0, width = 0, height = 0;
 
-	@nogc nothrow pure public:
+	@nogc nothrow public:
 	this(int width, int height, int size)
 	{
 		this.width = width;
 		this.height = height;
 		this.size = size;
 		buffer.ensureCapacity((width / size) * (height / size));
+		for(size_t i = 0; i < width; i++)
+			for(size_t j = 0; j < height; j++)
+				buffer.add(Tile!T(T(), false));
 	}
 
-	ref Tile!T opIndex(int x, int y)
+	pure:
+	Tile!T opIndex(int x, int y)
 	{
-		return buffer[(x / size) * height / size + (y / size)];
+		return valid(x, y) ? buffer[(x / size) * height / size + (y / size)] : INVALID_TILE;
 	}
-	
+
 	ref Tile!T opIndexAssign(Tile!T tile, int x, int y)
 	{
 		return buffer[(x / size) * height / size + (y / size)] = tile;
+	}
+
+	bool valid(int x, int y)
+	{
+		return x >= 0 && y >= 0 && x < width && y < height;
 	}
 
 	bool empty(int x, int y)
@@ -37,22 +49,24 @@ struct Tilemap(T)
 
 	bool empty(int x, int y, int width, int height)
 	{
-		for(int i = x; i < x + width; x += size)
-			for(int j = y; j < y + height; y += size)
-				if(!empty(x, y))
+		for(int i = x; i < x + width; i += size)
+			for(int j = y; j < y + height; j += size)
+				if(!empty(i, j))
 					return false;
 		return empty(x + width, y) && empty(x, y + height) && empty(x + width, y + height);
 	}
 
-	Vector!int moveContact(int x, int y, int width, int height, Vector!int speed)
-	{
-		//TODO: implement this
-	}
-
-	Vector!int slideContact(int x, int y, int width, int height, Vector!int speed)
-	{
-		auto x = moveContact(x, y, width, height, Vector!int(speed.x, 0));
-		auto y = moveContact(x, y, width, height, Vector!int(0, speed.y));
-		return Vector!int(x.x, y.y);
+	Vector!int slideContact(int x, int y, int width, int height, Vector!int v) {
+		if (empty(x + v.x, y + v.y, width, height)) {
+			return v;
+		} else {
+			while (!empty(x + v.x, y, width, height)) {
+				v.x /= 2;
+			}
+			while (!empty(x + v.x, y + v.y, width, height)) {
+				v.y /= 2;
+			}
+			return v;
+		}
 	}
 }
