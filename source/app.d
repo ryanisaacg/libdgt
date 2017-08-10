e4cd304da4342abdcc674a48548bdf7b3ab85c1c
Ryan Goldstein <ryanisaacg@gmail.com>
2017-08-10 00:01:40 -0400

    Added a demonstration of the tilemap

--- source/app.d
+++ source/app.d
@@ -7,6 +7,10 @@ void main()
 	WindowConfig config;
 	Window engine = new Window("Test title", 640, 480, config);
 	auto tex = engine.loadTexture("test.png");
+	int x = 100, y = 10;
+	auto map = Tilemap!bool(640, 480, 32);
+	map[96, 100] = Tile!bool(true, true);
+	map[128, 100] = Tile!bool(true, true);
 	while(engine.should_continue)
 	{
 		engine.begin(black);
@@ -18,6 +22,11 @@ void main()
 		engine.draw(red, Rectanglef(30, 30, 40, 40));
 		engine.draw(blue, Rectanglef(100, 100, 40, 40));
 		engine.draw(Color(0, 1, 0, 0.5), Circlef(100, 100, 32));
+
+		engine.draw(green, Rectanglef(x, y, 32, 32));
+		auto move = map.slideContact(x, y, 32, 32, Vector!int(1, 3));
+		x += move.x;
+		y += move.y;
 		engine.end();
 	}
 	engine.destroy();
