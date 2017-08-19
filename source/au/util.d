4455b4a25cab0dc02257308b3ad1ea2e76b3c741
Ryan Goldstein <ryanisaacg@gmail.com>
2017-08-18 21:12:30 -0400

    Added tests to util and fixed a bug

--- source/au/util.d
+++ source/au/util.d
@@ -20,9 +20,22 @@ int randi_range(int min, int max)
 }
 Vector!float randvectorf_range(Vector!float a, Vector!float b)
 {
-    return Vector!float(a.x + b.x, a.y + b.y);
+    return Vector!float(randf_range(a.x, b.x), randf_range(a.y, b.y));
 }
 Vector!int randvectori_range(Vector!int a, Vector!int b)
 {
-    return Vector!int(a.x + b.x, a.y + b.y);
+    return Vector!int(randi_range(a.x, b.x), randi_range(a.y, b.y));
+}
+unittest
+{
+    for(size_t i = 0; i < 1000; i++)
+    {
+        Vectori vector = randvectori_range(Vectori(-1, -1), Vectori(5, 5));
+        assert(vector.x >= -1 && vector.y >= -1 && vector.x < 5 && vector.y < 5);
+    }
+    for(size_t i = 0; i < 1000; i++)
+    {
+        Vectorf vector = randvectorf_range(Vectorf(-1, -1), Vectorf(5, 5));
+        assert(vector.x >= -1 && vector.y >= -1 && vector.x < 5 && vector.y < 5);
+    }
 }
