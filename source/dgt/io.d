36e7f96713c790bd95f7eea2474fc16b43ec53d9
Ryan Goldstein <ryanisaacg@gmail.com>
2017-08-31 09:03:36 -0400

    Documented the io module

--- source/dgt/io.d
+++ source/dgt/io.d
@@ -1,23 +1,28 @@
+///@nogc printing functions
 module dgt.io;
 import core.stdc.stdio;
 
 @nogc nothrow @trusted:
 
+///Print the integer to stdout
 void print(in int value)
 {
     printf("%d", value);
 }
 
+///Print the double to stdout
 void print(in double value)
 {
     printf("%f", value);
 }
 
+///Call the print function of the object
 void print(T)(in T obj)
 {
     obj.print();
 }
 
+///Print an array of items
 void print(T)(in T[] items)
 {
     print(T.stringof, "[");
@@ -30,11 +35,13 @@ void print(T)(in T[] items)
     print("]");
 }
 
+///Print a string (special case of an array)
 void print(T:char)(in T[] items)
 {
     printf("%s", items.ptr);
 }
 
+////Print some number of objects
 void print(T, A...)(in T obj, in A a)
 {
     print(obj);
@@ -42,6 +49,7 @@ void print(T, A...)(in T obj, in A a)
         print(val);
 }
 
+///Print some number of objects followed by a newline
 void println(A...)(in A values)
 {
     foreach(val; values)
