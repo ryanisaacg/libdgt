552c9c6d8602c8a6aed8e807ef339d685d6c411f
Ryan Goldstein <ryanisaacg@gmail.com>
2017-09-04 09:35:56 -0400

    Renamed the 'Map' structure to 'Level'
    This is for two reasons: Map and Tilemap are too similar and if the
    module is named 'level,' it makes sense for the most important structure
    to be named 'Level.'
    
    Resolves #47

--- source/app.d
+++ source/app.d
@@ -6,7 +6,7 @@ void main()
 {
 	WindowConfig config = { resizable : true, vsync : true };
 	Window engine = Window("Test title", 640, 480, config);
-	auto level = Map("example/example.json");
+	auto level = Level("example/example.json");
     auto tex = Texture("example/test.png");
 	scope(exit) tex.destroy();
 	auto map = Tilemap!bool(640, 480, 32);
