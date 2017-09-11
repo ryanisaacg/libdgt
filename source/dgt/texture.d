9dda0b04bfe273af68355d9fbdbd551f2006ceae
Ryan Goldstein <ryanisaacg@gmail.com>
2017-09-11 16:30:47 -0400

    Fixed internal format not being RGBA
    This also happens to fix font rendering.
    
    Resolves #56

--- source/dgt/texture.d
+++ source/dgt/texture.d
@@ -40,7 +40,7 @@ struct Texture
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexImage2D(GL_TEXTURE_2D, 0, format, w, h, 0, format, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
         id = texture;
         width = w;
