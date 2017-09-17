3ebf2ee96d080f48c46492284374f10632c3ae1f
Ryan Goldstein <ryanisaacg@gmail.com>
2017-09-17 18:13:34 -0400

    Added missing import for the camera

--- source/dgt/package.d
+++ source/dgt/package.d
@@ -1,4 +1,4 @@
 module dgt;
-public import dgt.array, dgt.color, dgt.font, dgt.geom, dgt.io, dgt.level, dgt.music,
+public import dgt.array, dgt.camera, dgt.color, dgt.font, dgt.geom, dgt.io, dgt.level, dgt.music,
     dgt.sprite, dgt.sound, dgt.texture, dgt.tilemap, dgt.ui, dgt.window;
 public import dgt.gl_backend : DEFAULT_VERTEX_SHADER, DEFAULT_FRAGMENT_SHADER;
