5f736b0056dd303205ef30bc7fbaacd54a6cc7f9
Ryan Goldstein <ryanisaacg@gmail.com>
2017-09-18 16:17:29 -0400

    Added arbitrary camera transformations
    Resolves #63

--- source/dgt/camera.d
+++ source/dgt/camera.d
@@ -8,13 +8,15 @@ struct Camera
     const Transform project, unproject, opengl;
 
     ////Create a camera with a given window size and view of the world
-    pure @nogc nothrow this(in Rectangle windowSize, in Rectangle world)
+    pure @nogc nothrow this(in Rectangle windowSize, in Rectangle world, in Transform cameraTransform = Transform())
     {
-        project = Transform.translate(-windowSize.topLeft)
-            * Transform.scale(Vector(windowSize.width / world.width, windowSize.height / world.height).inverse)
-            * Transform.translate(world.topLeft);
-        unproject = project.inverse;
-        opengl = Transform.translate(-world.topLeft)
+        unproject = cameraTransform
+            * Transform.translate(-world.topLeft)
+            * Transform.scale(Vector(world.width / windowSize.width, world.height / windowSize.height).inverse)
+            * Transform.translate(windowSize.topLeft);
+        project = unproject.inverse;
+        opengl = cameraTransform
+            * Transform.translate(-world.topLeft)
             * Transform.scale(world.size.inverse * 2)
             * Transform.translate(Vector(-1, -1))
             * Transform.scale(Vector(1, -1));
@@ -35,4 +37,11 @@ unittest
     const cam = Camera(Rectangle(0, 0, 100, 100), Rectangle(50, 50, 50, 50));
     const worldTop = Vector(50, 50);
     assert(cam.opengl * worldTop == Vector(-1, 1));
+}
+unittest
+{
+    const cam = Camera(Rectangle(0, 0, 10, 10), Rectangle(0, 0, 10, 10),
+            Transform.rotate(-90));
+    const projected = cam.project * Vector(5, 0);
+    assert(projected == Vector(0, 5));
 }
