c3f3c71e8ac3a805a3100d1dd22a1ad6f9a66a0d
Ryan Goldstein <ryanisaacg@gmail.com>
2017-09-24 16:50:05 -0400

    Added support for loading Spine texture atlases
    Resolves #38
    
    Squashed commit of the following:
    
    commit 61cfd053121910aa0d8cdb59dbb5cef90744ce16
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Sun Sep 24 16:47:26 2017 -0400
    
        Added destructor to unit test
    
    commit 633ef2fd9ba79a7c6f4b68371bc7d134950d6efe
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Sun Sep 24 16:39:40 2017 -0400
    
        Refactored and added documentation
    
    commit b6a1121cdd1b4aad4e1ae713d977d35ee1a07741
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Sun Sep 24 16:24:10 2017 -0400
    
        Fixed bug where atlas textures didn't draw
    
        There was a use-after-free of the texture region names, so the search would always fail
    
    commit f7ebf068b235e3c086a97bb4d2c834aeb35ff4a3
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Sat Sep 23 11:48:58 2017 -0400
    
        Switched to atlas-based image loading
    
    commit 90ff434dde10326be2d48cb793d3b779306ea9d7
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Sat Sep 23 11:48:45 2017 -0400
    
        Refactored Atlas loading
    
        Atlas loading no longer requires GC and the Texture mocked loading is much simpler.
    
    commit 7569ccab33aad24fbdfff6a2e83117fcfc9258d1
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Sat Sep 23 11:46:39 2017 -0400
    
        Created an example texture atlas
    
    commit d3e0029d1c13d7ed61acfe1c87d1cfe86bb1131e
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Thu Sep 21 12:48:18 2017 -0400
    
        Added texture rotation to support rotated atlas textures
    
    commit 7c43adbd5a28ff480d7de767fa8f04927c60ceb7
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Thu Sep 21 12:47:46 2017 -0400
    
        Added a rotated image to the test atlas
    
    commit bb6f71e7a8bd06095304f288139388fb98fa1a28
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Thu Sep 21 12:32:25 2017 -0400
    
        Parsed the position and size of the regions
    
    commit 48a5bd0068b5197fe7e58ef20021b67a583624cd
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Thu Sep 21 12:32:04 2017 -0400
    
        Added a function parse positive ints from strings
    
    commit 5ed6232ff5fd93b702b455ceb6b5144d359507a7
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Thu Sep 21 12:31:34 2017 -0400
    
        Modified the atlas to differentiate the regions
    
    commit 4ec9730aa27d002cb263bb93c43334e2339fca87
    Merge: 0fa8a82 acfe5da
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Thu Sep 21 12:05:07 2017 -0400
    
        Merge branch 'master' into texture-atlas
    
    commit 0fa8a8262a8ea6ab479cd9dcd049381e0cce42a5
    Author: Ryan Goldstein <ryanisaacg@gmail.com>
    Date:   Wed Sep 20 17:11:25 2017 -0400
    
        Fully read the texture atlas string into memory

--- source/dgt/texture.d
+++ source/dgt/texture.d
@@ -1,12 +1,16 @@
 module dgt.texture;
+
 import derelict.sdl2.sdl, derelict.sdl2.image;
 import derelict.opengl;
 import dgt.array : Array;
-import dgt.io;
-import dgt.geom : Rectangle;
-import dgt.util : nullTerminate;
+import dgt.io : println;
+import dgt.geom : Vector, Rectangle;
+import dgt.util;
 
-import core.stdc.string;
+import std.path : dirName;
+import std.string : indexOf;
+
+import core.stdc.string : strlen;
 
 ///The format of each pixel in byte order
 enum PixelFormat : GLenum
@@ -22,17 +26,20 @@ A drawable texture which can also be a region of a larger texture
 */
 struct Texture
 {
-    package uint id;
-    private:
+    public uint id;
+    public:
     int width, height;
     Rectangle region;
+    package bool rotated = false;
 
     @disable this();
+
 
     @nogc nothrow public:
     ///Create a Texture from data in memory
     this(ubyte* data, int w, int h, PixelFormat format)
     {
+        println("Size: ", w, ":", h);
         GLuint texture;
         glGenTextures(1, &texture);
         glBindTexture(GL_TEXTURE_2D, texture);
@@ -47,23 +54,33 @@ struct Texture
         height = h;
         region = Rectangle(0, 0, w, h);
     }
-
-    ///Load a texture from a file with a given path
-    this(string name)
+    
+    //Mock or don't mock the constructor
+    version(unittest)
     {
-        auto nameNullTerminated = nullTerminate(name);
-        SDL_Surface* surface = IMG_Load(nameNullTerminated.ptr);
-        nameNullTerminated.destroy();
-        if (surface == null)
+        this(string name)
         {
-            auto buffer = IMG_GetError();
-            println("Image loading error: ", buffer[0..strlen(buffer)]);
-            this(null, 0, 0, PixelFormat.RGB);
         }
-        else
+    }
+    else
+    {
+        ///Load a texture from a file with a given path
+        this(string name)
         {
-            this(surface);
-            SDL_FreeSurface(surface);
+            auto nameNullTerminated = nullTerminate(name);
+            SDL_Surface* surface = IMG_Load(nameNullTerminated.ptr);
+            nameNullTerminated.destroy();
+            if (surface == null)
+            {
+                auto buffer = IMG_GetError();
+                println("Image loading error: ", buffer[0..strlen(buffer)]);
+                this(null, 0, 0, PixelFormat.RGB);
+            }
+            else
+            {
+                this(surface);
+                SDL_FreeSurface(surface);
+            }
         }
     }
 
@@ -84,19 +101,14 @@ struct Texture
         this(cast(ubyte*)sur.pixels, sur.w, sur.h, format);
     }
 
-    ///Remove the texture from GPU memory
-    void destroy()
-    {
-        glDeleteTextures(1, &id);
-    }
-
     pure:
     ///Get a texture that represents a region of a larger texture
-    Texture getSlice(Rectangle region)
+    Texture getSlice(Rectangle region, bool rotated = false) const
     {
         Texture tex = this;
         tex.region = Rectangle(this.region.x + region.x,
                 this.region.y + region.y, region.width, region.height);
+        tex.rotated = rotated;
         return tex;
     }
     ///Get the width of the source image
@@ -105,4 +117,108 @@ struct Texture
     @property int sourceHeight() const { return height; }
     ///Get the size of the texture's region
     @property Rectangle size() const { return region; }
+}
+
+/**
+A structure that loads and stores texture atlases
+
+The loader assumes the input file is in the Spine format: http://esotericsoftware.com/spine-atlas-format
+*/
+struct Atlas
+{
+    private:
+    Array!Texture pages, regions;
+    Array!string regionNames;
+    Array!char contents;
+
+    @disable this();
+
+    nothrow @nogc:
+    ///Load the atlas and the textures from the given path
+    this(in string atlasPath)
+    {
+        pages = Array!Texture(2);
+        regions = Array!Texture(32);
+        regionNames = Array!string(32);
+        contents = readFileToBuffer(atlasPath);
+        string text = contents.array;
+        auto texturePath = Array!char(atlasPath.length * 2);
+        scope(exit) texturePath.destroy();
+        while(text.length > 0)
+        {
+            texturePath.clear();
+            string relativeTexturePath = text.nextline(text);
+            const atlasPathDir = dirName(atlasPath);
+            foreach(character; atlasPathDir) texturePath.add(character);
+            texturePath.add('/');
+            foreach(character; relativeTexturePath) texturePath.add(character);
+            const page = Texture(texturePath.array);
+            pages.add(page);
+            //ignore the size, format, filter, and repeat lines of the format
+            for(int i = 0; i < 4; i++)
+                text.nextline(text);
+            auto regionName = text.nextline(text);
+            while(regionName.length > 0)
+            {
+                auto propertyLine = text.nextline(text);
+                bool rotate;
+                Vector position, size;
+                while(propertyLine.length > 0 && propertyLine[0] == ' ')
+                {
+                    propertyLine = propertyLine.trimLeft;
+                    const colonIndex = propertyLine.indexOf(':');
+                    const property = propertyLine[0..colonIndex];
+                    const value = propertyLine[colonIndex + 1..propertyLine.length].trimLeft;
+                    if(property == "rotate")
+                    {
+                        rotate = (value == "true");
+                    } else if(property == "xy")
+                    {
+                        const x = parsePositiveInt(value[0..value.indexOf(',')]);
+                        const y = parsePositiveInt(value[value.indexOf(',') + 1..value.length].trimLeft);
+                        position = Vector(x, y);
+                    } else if(property == "size")
+                    {
+                        const x = parsePositiveInt(value[0..value.indexOf(',')]);
+                        const y = parsePositiveInt(value[value.indexOf(',') + 1..value.length].trimLeft);
+                        size = Vector(x, y);
+                    }
+                    propertyLine = text.nextline(text);
+                }
+                regions.add(page.getSlice(Rectangle(position, size), rotate));
+                regionNames.add(regionName);
+                regionName = propertyLine;
+            }
+        }
+    }
+
+    @nogc:
+    ///Get the texture by the name it has in the atlas
+    pure Texture opIndex(in string regionName, 
+            in Texture notFound = Texture(null, 0, 0, PixelFormat.RGB)) const
+    {
+        for(uint i = 0; i < regionNames.length; i++)
+            if(regionNames[i] == regionName)
+                return regions[i];
+        return notFound;
+    }
+
+    ///Free the memory and textures associated with the atlas
+    void destroy()
+    {
+        pages.destroy();
+        regions.destroy();
+        contents.destroy();
+    }
+}
+
+unittest
+{
+    auto atlas = Atlas("test.atlas");
+    scope(exit) atlas.destroy();
+    assert(atlas.regionNames[0] == "bg-dialog");
+    assert(atlas.regionNames[1] == "bg-dialog2");
+    assert(atlas.regions[0].size.topLeft == Vector(519, 223));
+    assert(atlas.regions[0].size.size == Vector(21, 42));
+    assert(atlas.regions[1].rotated);
 }
--- source/dgt/util.d
+++ source/dgt/util.d
@@ -1,8 +1,14 @@
 module dgt.util;
-import core.stdc.math, core.stdc.stdlib;
+import core.stdc.math, core.stdc.stdlib, core.stdc.stdio;
+
+import std.algorithm : canFind;
+import std.algorithm.comparison : equal;
+import std.math;
+import std.string : indexOf;
 
 import dgt.array;
 import dgt.geom;
+import dgt.io : println;
 
 @nogc nothrow:
 
@@ -35,6 +41,60 @@ Array!char nullTerminate(in string str)
     nameNullTerminated.add('\0');
     return nameNullTerminated;
 }
+///Get a slice of the next line of the string
+pure string nextline(in string str, out string rest)
+{
+    auto index = str.indexOf('\n');
+    if(index == -1)
+    {
+        index = str.length;
+        rest = "";
+    }
+    else
+    {
+        rest = str[index + 1..str.length];
+    }
+    return str[0..index];
+}
+///Get a slice of the string excluding whitespace on the left
+pure string trimLeft(in string str)
+{
+    int i = 0;
+    for(; i < str.length; i++)
+        if(str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\r')
+            break;
+    return str[i..str.length];
+}
+///Parse a string that represents a positive int into an int
+pure int parsePositiveInt(string str)
+{
+    //TODO: use a stdc function like snprintf
+	int value = 0;
+	for(int digit = 0; digit < str.length; digit++)
+	{
+		import core.stdc.stdio;
+		value += cast(int)std.math.pow(10, digit) * (str[str.length - 1 - digit] - '0');
+	}
+	return value;
+}
+///Read an file into a buffer of characters
+Array!char readFileToBuffer(in string pathName)
+{
+    auto terminated = nullTerminate(pathName);
+    FILE* file = fopen(terminated.ptr, "r".ptr);
+    terminated.destroy();
+    if(file == null)
+    {
+        println("Failed to load file ", pathName);
+        return Array!char(0);
+    }
+    auto contents = Array!char(1024);
+    int next;
+    //Read the file into memory
+    while((next = fgetc(file)) != EOF)
+        contents.add(cast(char)next);
+    return contents;
+}
 unittest
 {
     for(size_t i = 0; i < 1000; i++)
@@ -49,4 +109,35 @@ unittest
     auto expected = "Test string\0";
     for(size_t i = 0; i < expected.length; i++)
         assert(str[i] == expected[i]);
+}
+unittest
+{
+    auto manyLines = "First line
+Second line
+
+Fourth line";
+    const firstLine = manyLines.nextline(manyLines);
+    assert(firstLine == "First line");
+    const secondLine = manyLines.nextline(manyLines);
+    assert(secondLine == "Second line");
+    const thirdLine = manyLines.nextline(manyLines);
+    assert(thirdLine == "");
+    const fourthLine = manyLines.nextline(manyLines);
+    assert(fourthLine == "Fourth line");
+}
+unittest
+{
+    const noTrim = "Text";
+    const trimSpace = " Text";
+    const trimVariety = "
+
+    Text";
+    assert(noTrim == noTrim.trimLeft);
+    assert(noTrim == trimSpace.trimLeft);
+    assert(noTrim == trimVariety.trimLeft);
+}
+unittest
+{
+    assert(parsePositiveInt("0") == 0);
+    assert(parsePositiveInt("234") == 234);
 }
